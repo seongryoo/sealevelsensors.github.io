06bf97a400f84412f8b24458bf6496f5a7bb9177
Seong <sryoo6@gatech.edu>
2020-05-19 12:18:47 -0400

    Improve dynamic ARIA attributes on interactions

--- js/nav.js
+++ js/nav.js
@@ -5,43 +5,56 @@ const burger = document.getElementById('hamburgerButton');
 const dark = document.getElementById('darkOverlay');
 let burgerStyles;
 
-/* Helper method for checking if hamburger menu is open*/
-
-function ifMenuOpen() {
-  burgerStyles = window.getComputedStyle(burger);
-  let burgerDisplayed = burgerStyles.getPropertyValue('display');
-  return burgerDisplayed == 'none' || header.classList.contains('open');
-}
-function ifMenuClosed() {
-  burgerStyles = window.getComputedStyle(burger);
-  let burgerDisplayed = burgerStyles.getPropertyValue('display');
-  return burger.style.display == 'none' || !header.classList.contains('open');
+function isMenuOpen() {
+  return strudel.isStyle('#hamburgerButton', 'display', 'none') ||
+         strudel.hasClass('#headerOpen', 'open');
 }
 
-/* Updates all interactive aria labels */
+let strudelNav = strudel.query(isMenuOpen)
+  .watch('#hamburgerButton', 'display')
+  .watch('#headerOpen', 'class')
 
-function updateAttributes() {
-  makeFocusable('logo-link', ifMenuClosed);
-  makeFocusable('navlink', ifMenuOpen);
-  updateAriaHidden('logo-link', ifMenuClosed);
-  updateAriaHidden('navlink', ifMenuOpen);
-  updateHamburgerAria();
-}
+  .reaction('.logo-link')
+    .set('tabIndex', -1)
+    .set('aria-hidden', true)
+      .else()
+    .set('tabIndex', 0)
+    .remove('aria-hidden')
+
+  .reaction('.navlink')
+    .set('tabIndex', 0)
+    .remove('aria-hidden')
+      .else()
+    .set('tabIndex', -1)
+    .set('aria-hidden', true)
+
+  .reaction('#hamburgerButton')
+    .set('aria-label', 'Close hamburger menu')
+    .set('aria-pressed', 'true')
+      .else()
+    .set('aria-label', 'Open hamburger menu')
+    .set('aria-pressed', 'false');
+
 
 /* Handles actual toggling of header class */
 
 function flipNav() {
   header.classList.toggle('open');
-  updateAttributes();
 }
 function offNav() {
   header.classList.remove('open');
-  updateAttributes();
 }
 
-/* Update attributes is called at the document load */
-
-updateAttributes();
+function enterWrapper(func) {
+  return function(event) {
+    if (event.key == " " || event.key == "Spacebar") {
+      event.preventDefault();
+      func();
+    } else if (event.key == "ENTER") {
+      func();
+    }
+  }
+}
 
 /* Clicking or pressing enter on burger will toggle nav */
 burger.addEventListener('click', flipNav);
@@ -50,13 +63,3 @@ burger.addEventListener('keydown', enterWrapper(flipNav));
 /* Clicking outside of navbar or changing window -> close nav*/
 dark.addEventListener('click', offNav);
 window.addEventListener('resize', offNav);
-
-function updateHamburgerAria() {
-  if (header.classList.contains('open')) {
-    burger.setAttribute('aria-label', 'Close hamburger menu');
-    burger.setAttribute('aria-pressed', 'true');
-  } else {
-    burger.setAttribute('aria-label', 'Open hamburger menu');
-    burger.setAttribute('aria-pressed', 'false');
-  }
-}
